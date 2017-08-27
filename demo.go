0bedf83239b5e45d818d3d4b7faa2fb815b413a7
Doug Campbell <dcampbell@gogoair.com>
2017-08-26 20:00:53 -0500

    added html templating

--- demo.go
+++ demo.go
@@ -1,15 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
+const index = `
+<html>
+	<head>
+	</head>
+	<body style="background-color:powderblue;">
+		<center>
+		<img src="https://secure.meetupstatic.com/photos/event/9/a/highres_436080154.jpeg">
+		<h1>Hello!</h1>
+		</center>
+	</body>
+</html>
+`
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	t, _ := template.New("index").Parse(index)
+	t.Execute(w, nil)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8000", nil)
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
